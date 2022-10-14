ee4b09dc91ec0034c7f5cad8078ec03848869812
ealgera <ericalgera@gmail.com>
2022-10-14 17:38:53 +0200

    Info op login- en index-screen

--- auth-code-authlib.py
+++ auth-code-authlib.py
@@ -16,29 +16,27 @@ client = OAuth(app)
 @app.route("/")
 def index():
 
-    client.register(
-        name            = 'azure_ad',
-        client_id       = app_config.AAD_CLIENT_ID,
-        client_secret   = app_config.AAD_CLIENT_SECRET,
-        access_token_url= app_config.AAD_ACCESS_TOKEN_URL,
-        access_token_params=None,
-        authorize_url   = app_config.AAD_AUTHORIZE_URL,
-        authorize_params= None,
-        api_base_url    = app_config.GRAPH_API_BASE,
-        client_kwargs   = {
-            'scope': app_config.SCOPE
-        },
-        server_metadata_url=f"https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
-        # issuerURL= f"https://login.microsoftonline.com/{app_config.AAD_TENANT_ID}/v2.0",
-        # jwks_uri = "https://login.microsoftonline.com/common/discovery/v2.0/keys" # -> zit in de well-know config
-    )
+    if not session.get("tokens"):            # Geen 'tokens' in de sessie, dan eerst aanmelden via de /login route
+        client.register(
+            name            = 'azure_ad',
+            client_id       = app_config.AAD_CLIENT_ID,
+            client_secret   = app_config.AAD_CLIENT_SECRET,
+            access_token_url= app_config.AAD_ACCESS_TOKEN_URL,
+            access_token_params=None,
+            authorize_url   = app_config.AAD_AUTHORIZE_URL,
+            authorize_params= None,
+            api_base_url    = app_config.GRAPH_API_BASE,
+            client_kwargs   = {
+                'scope': app_config.SCOPE
+            },
+            server_metadata_url=f"https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration",
+        )
 
-    if not session.get("user"):            # Geen 'user' in de sessie, dan eerst aanmelden via de /login route en sessie dict opbouwen
         return redirect(url_for("login"))
 
     return render_template(
         "index.html",
-        user=session["user"],
+        user=session["tokens"],
         my_session=session,
         app_id=app_config.AAD_CLIENT_ID,
         app_secret=app_config.AAD_CLIENT_SECRET
@@ -48,18 +46,26 @@ def index():
 def login():
     '''
     '''
-    azure_ad     = client.create_client("azure_ad")
+    azure_ad = client.create_client("azure_ad")
+    # print(f"[info] <login> AZURE_AD Client: {type(azure_ad)}")
+    
+    # for k, v in vars(azure_ad).items():
+    #     print(f"[info] <login> {k}: {v}")
+
     redirect_uri = "http://localhost:5000" + app_config.REDIRECT_PATH  # url_for(app_config.REDIRECT_PATH)
 
-    return azure_ad.authorize_redirect(redirect_uri)
+    auth_uri = azure_ad.authorize_redirect(redirect_uri)
+    auth_url = vars(auth_uri).get('headers').get('Location')
 
-    # return render_template(
-    #     "login.html", 
-    #     auth_url=app_config.AAD_AUTHORIZE_URL,
-    #     app_id=app_config.AAD_CLIENT_ID,
-    #     app_secret=app_config.AAD_CLIENT_SECRET,
-    #     session_info = session
-    # )
+    # return azure_ad.authorize_redirect(redirect_uri)
+
+    return render_template(
+        "login.html", 
+        auth_url=auth_url,
+        app_id=app_config.AAD_CLIENT_ID,
+        app_secret=app_config.AAD_CLIENT_SECRET,
+        auth_info = vars(auth_uri)
+    )
 
 @app.route("/logout")
 def logout():
@@ -72,7 +78,7 @@ def logout():
 @app.route(app_config.REDIRECT_PATH)  # Leidt naar: /getAToken
 def authorized():
     '''
-    Issuer validatie in /authlib/jose/rfc7519/claims.py uitgezet in functie _validate_claim_value. 
+    !!! Issuer validatie in /authlib/jose/rfc7519/claims.py uitgezet in functie _validate_claim_value. 
     Zie ook: https://github.com/MicrosoftDocs/azure-docs/issues/38427 en: https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration
     MS geeft als 'issuer' aan: "https://login.microsoftonline.com/{tenantid}/v2.0". Dit is niet volgens de OIDC rfc.
     De code vergelijkt "https://login.microsoftonline.com/<werkelijk id>/v2.0" met "https://login.microsoftonline.com/{tenantid}/v2.0"
@@ -82,6 +88,7 @@ def authorized():
 
     try:
         token = client.azure_ad.authorize_access_token()
+        session["tokens"] = token
     except Exception as e:
         print(f"\n *** FOUT ***")
         print(e)
@@ -90,8 +97,8 @@ def authorized():
     for token_key, token_value in token.items():
         print(f"[info] -- item {token_key} : {token_value}")
 
-    return redirect(url_for("logout"))
-    # return redirect(url_for("index"))
+    # return redirect(url_for("logout"))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run()
